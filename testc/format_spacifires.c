2f0a166925a1ef4083b9ab1e3b461cfe35ea9e13
shekh farid <shekhfarid1974@gmail.com>
2024-02-06 15:34:45 +0600

    another commit

--- testc/format_spacifires.c
+++ testc/format_spacifires.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+/*#include<stdio.h>
 
 int main() {
 
@@ -11,4 +11,61 @@ printf("%f\n", MyFloatNum);
 printf("%c\n", MyChar);
 return 0;
 
+}
+*/
+/*#include<stdio.h>
+
+int main() {
+
+int myNum = 15;
+int myOtherNum = 23;
+
+//assign the value of myOtherNum to myNum
+myNum = myOtherNum;
+
+printf("%d", myNum);
+
+return 0;
+
+}
+*/
+/*
+#include<stdio.h>
+
+int main(){
+int myNum = 15;
+int myOtherNum;
+myOtherNum = myNum;
+printf("%d", myOtherNum);
+return 0;
+
+}
+*/
+
+/*
+//Add Variables Together
+#include<stdio.h>
+int main(){
+int x =5;
+int y =6;
+int sum = x+y;
+printf("%d", sum);
+return 0;
+
+}
+*/
+
+#include<stdio.h>
+
+public class Main {
+  public static void main(String[] args) {
+    String message = null;
+    try {
+      message = message.concat("Hello, World!");
+    } catch (NullPointerException e) {
+      message = "Attempt to modify null object";
+    } finally {
+      System.err.println(message);
+    }
+  }
 }
