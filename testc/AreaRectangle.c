14d961955dcf935d5be1f2ed409027d52041a7b3
shekh farid <shekhfarid1974@gmail.com>
2024-02-10 23:17:34 +0600

    multivar

---
+++ testc/AreaRectangle.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+
+int length = 4;
+int width = 6;
+int area;
+
+area = length * width;
+
+printf("Length is: %d\n", length);
+printf("width is: %d\n", width);
+printf("area of rectangle is: %d", area);
+
+return 0;
+}
---
+++ testc/multilpleVar.c
@@ -0,0 +1,25 @@
+/*#include<stdio.h>
+int main(){
+int x,y,z;
+x=y=z=50;
+printf("%d", x+y+z);
+return 0;
+}
+*/
+
+#include<stdio.h>
+int main(){
+
+//student data
+int StudentId = 15;
+int StudentAge = 23;
+float StudentFee = 75.25;
+char StudentGrade = 'B';
+
+//print Variables
+printf("Student Id: %d\n", StudentId);
+printf("Student Age: %d\n", StudentAge);
+printf("Student Fee: %f\n", StudentFee);
+printf("Student Grade: %c", StudentGrade);
+return 0;
+}
